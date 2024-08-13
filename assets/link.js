44da37984f0634fc89a18ff75abd691bf7c1f2a9
yangbin <yb@qq.com>
2024-08-13 23:50:04 +0800

    getparams

--- assets/link.js
+++ assets/link.js
@@ -3,6 +3,19 @@
 // 	return "https://appvmstore.appvm.store/click"
 // }
 
+function getOneParam(key) {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split("=");
+		//if(pair[0] == variable){return pair[1];}
+		if (pair[0] == key) {
+			return decodeURI(pair[1]);
+		} //解决中文乱码
+	}
+	return (false);
+}
+
 function getClickUrl() {
 	let domain = getOneParam('domain')
 	
